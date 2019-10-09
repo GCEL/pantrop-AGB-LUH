ae986dbd41932f1aed80ca4f1a4e457f1d1b2711
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-10-09 10:11:43 +0100

    things should be ok now with randomized search. full test now

--- pt2a_RFR_RandomizedSearch.py
+++ pt2a_RFR_RandomizedSearch.py
@@ -24,13 +24,6 @@ y = xr.open_rasterio('/disk/scratch/local.2/jexbraya/AGB/Avitable_AGB_Map_0.25d.
 #split train and test subset, specifying random seed
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.25, random_state=26)
 
-#define the parameters for the gridsearch
-random_grid = { "bootstrap":[True,False],
-                "n_estimators": np.linspace(200,2000,10,dtype='i'),
-                "max_depth": list(np.linspace(5,100,20,dtype='i'))+[None],
-                "max_features": np.linspace(.1,1.,10),
-                "min_samples_leaf": np.linspace(5,50,10,dtype='i') }
-
 #create the random forest object and fit it out of the box
 rf = RandomForestRegressor(n_jobs=20,random_state=26)
 # rebalance the training set
@@ -38,12 +31,19 @@ X_train_resampled,y_train_resampled = balance_training_data(X_train,y_train,n_bi
 # fit model
 rf.fit(X_train_resampled,y_train_resampled)
 #save the mse for the cal / val
-oob_cal = mean_squared_error(y_train,rf.predict(X_train))
-oob_val = mean_squared_error(y_test,rf.predict(X_test))
+oob_cal = np.sqrt(mean_squared_error(y_train,rf.predict(X_train)))
+oob_val = np.sqrt(mean_squared_error(y_test,rf.predict(X_test)))
 print('Out of the box RMSE:\n\tcalibration score = %f\n\tvalidation_score = %f' % (oob_cal,oob_val))
 
 # perform a randomized search on hyper parameters using training subset of data
-n_iter = 100
+
+#define the parameters for the search
+random_grid = { "bootstrap":[True,False],
+                "n_estimators": np.linspace(200,2000,10,dtype='i'),
+                "max_depth": list(np.linspace(5,100,20,dtype='i'))+[None],
+                "max_features": np.linspace(.1,1.,10),
+                "min_samples_leaf": list(np.linspace(5,50,10,dtype='i'))+[100,150,200,250,300] }
+n_iter = 200
 fold=3
 RandomizedSearchResults = {}
 RandomizedSearchResults['params']=[]
@@ -51,17 +51,20 @@ RandomizedSearchResults['scores']=[]
 RandomizedSearchResults['mean_scores']=[]
 best_score = np.inf
 for ii in range(0,n_iter):
+    print('{0}\r'.format(ii),end='\r')
     params={}
     for pp,param in enumerate(random_grid.keys()):
         params[param] = np.random.choice(random_grid[param])
+    params['n_jobs'] = 30
     RandomizedSearchResults['params'].append(params)
-    scores = balanced_cv(params,X_train,y_train,cv=fold,random_state=2097)
+    scores = balanced_cv(params,X_train,y_train,cv=fold,target=12600,random_state=2097)
     RandomizedSearchResults['scores'].append(scores)
     RandomizedSearchResults['mean_scores'].append(np.mean(scores))
     if RandomizedSearchResults['mean_scores'][ii]<best_score:
         best_score = RandomizedSearchResults['mean_scores'][ii]
         print('\tNew Best RMSE: %.06f' % (best_score))
         print(params)
+        print('\n')
 
 np.savez(RandomizedSearchResults,'/disk/scratch/local.2/dmilodow/pantrop_AGB_LUH/saved_algorithms/rf_random.npy')
 
