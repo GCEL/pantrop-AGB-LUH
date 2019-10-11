5841d7a9d11d61c3db6cd525b4d1ce70a66151f2
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-10-11 15:22:17 +0100

    updating final fit script to account for previous changes

--- pt4_RFR_final_fit.py
+++ pt4_RFR_final_fit.py
@@ -11,10 +11,22 @@ from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
 import pandas as pd
 
-pca = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/pca_pipeline.pkl')
+pca = joblib.load('/disk/scratch/local.2/dmilodow/pantrop-AGB-LUH/saved_algorithms/pca_pipeline.pkl')
 
-#load the fitted  the fitted rf_grid
-rf = joblib.load('/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/rf_grid.pkl').best_estimator_
+#load the fitted rf_grid
+rf_grid = np.load('/disk/scratch/local.2/dmilodow/pantrop_AGB_LUH/saved_algorithms/rf_grid.npy')[()]
+# Construct best-fitting random forest model
+idx = np.argmin(rf_grid['mean_test_score'])
+rf_best = RandomForestRegressor(bootstrap=rf_grid['params'][idx]['bootstrap'],
+            max_depth= rf_grid['params'][idx]['max_depth'],
+            max_features=rf_grid['params'][idx]['max_features'],
+            min_samples_leaf=rf_grid['params'][idx]['min_samples_leaf'],
+            n_estimators=rf_grid['params'][idx]['n_estimators'],
+            n_jobs=30,
+            oob_score=True,
+            random_state=26,
+            )
+
 
 #refit to whole dataset - get predictors and targets
 predictors,landmask = get_predictors(y0=2000,y1=2009)
@@ -25,6 +37,6 @@ unc = xr.open_rasterio('/disk/scratch/local.2/jexbraya/AGB/Avitable_AGB_Uncertai
 lvls = ['mean','upper','lower']
 for aa, agb in enumerate([med,med+unc,med-unc]):
     agb[agb<0] = 0
-    rf.fit(X,agb)
-    print(mean_squared_error(rf.predict(X),agb))
-    joblib.dump(rf,'/disk/scratch/local.2/jexbraya/pantrop-AGB-LUH/saved_algorithms/rf_%s.pkl' % lvls[aa])
+    rf_best.fit(X,agb)
+    print(mean_squared_error(rf_best.predict(X),agb))
+    joblib.dump(rf_best,'/disk/scratch/local.2/dmilodow/pantrop-AGB-LUH/saved_algorithms/rf_%s.pkl' % lvls[aa])
