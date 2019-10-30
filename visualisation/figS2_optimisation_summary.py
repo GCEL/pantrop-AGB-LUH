745457b057a6ca4a00b42a26f4b470d29a04788e
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-10-30 12:24:08 +0000

    plotting optimisation summary results

--- visualisation/figS2_optimisation_summary.py
+++ visualisation/figS2_optimisation_summary.py
@@ -28,8 +28,8 @@ grid_res = pd.DataFrame(rf_grid['params'])
 #get the scores
 grid_res['mean_train_score'] = rf_grid['mean_train_score']
 grid_res['mean_test_score'] = rf_grid['mean_test_score']
-grid_res['grad_test'] = rf_grid['gradient_test']
-grid_res['grad_train'] = rf_grid['gradient_train']
+#grid_res['grad_test'] = rf_grid['gradient_test']
+#grid_res['grad_train'] = rf_grid['gradient_train']
 grid_res['ratio_score'] = grid_res['mean_test_score'] / grid_res['mean_train_score']
 
 cv_res = cv_res.append(grid_res)
@@ -41,16 +41,18 @@ row_vars = ['mean_test_score','mean_train_score','ratio_score','grad_test','grad
 row_labels = ['validation\nRMSE\nMg ha$^{-1}$', 'calibration\nRMSE\nMg ha$^{-1}$', 'cal:val\nRMSE ratio', 'validation\ngradient', 'calibration\ngradient']
 
 sns.set()
-fig,axes = plt.subplots(nrows=5,ncols=5,figsize=[8,8], sharex='col',sharey='row')
+fig,axes = plt.subplots(nrows=5,ncols=5,figsize=[8,9], sharex='col',sharey='row')
 for ii,axes_row in enumerate(axes):
     for jj, ax in enumerate(axes_row):
         ax.plot(cv_res[col_vars[jj]],cv_res[row_vars[ii]],'.',ms=1)
         if jj==0:
             ax.set_ylabel(row_labels[ii],fontsize=8)
-            ax.set_yticklabels(labels=ax.get_yticklabels(),fontsize=8)
+            for tick in ax.yaxis.get_major_ticks():
+                tick.label.set_fontsize(8)
 for jj, ax in enumerate(axes[-1]):
-    ax.set_xlabel(col_labels[jj],fontsize=8)
-    ax.set_xticklabels(labels=ax.get_xticklabels(),fontsize=8)
+    for tick in ax.xaxis.get_major_ticks():
+        tick.label.set_fontsize(8)
+        ax.set_xlabel(col_labels[ii],fontsize=8)
 
 fig.tight_layout()
 fig.show()
